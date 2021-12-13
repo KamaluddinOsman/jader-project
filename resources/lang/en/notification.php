7390d0ec5b6486cca238ec6cbebd38b4230fe709
Kamaluddin Osman <kamaluddin.osman@gmail.com>
2021-12-14 01:06:49 +0400

    Notification to (test) stage

--- resources/lang/en/notification.php
+++ resources/lang/en/notification.php
@@ -7,4 +7,11 @@
         'body'              => 'Body',
         'type'              => 'Type',
         'send'              => 'Send',
+
+        'client'            => 'Client',
+        'captain'           => 'Captian',
+        'institution'       => 'Institution',
+
+        //Messages
+        'sentSuccessfully'  => 'The notification has been sent successfully',
     ];
--- resources/views/dashboard/pages/notification/create.blade.php
+++ resources/views/dashboard/pages/notification/create.blade.php
@@ -3,13 +3,6 @@
     @section('page-title')
     {{ __('notification.notification') }} | {{ __('auth.bageTitle') }}             
     @endsection
-    <!-- DataTables -->
-    <link href="{{ asset('dashboard/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('dashboard/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
-
-    <!-- Responsive datatable examples -->
-    <link href="{{ asset('dashboard/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
-
 @endsection
 @section('content')
     <div class="page-content">
@@ -30,58 +23,50 @@
             </div>
         </div>
         <!-- end page title -->
+        
+        @include('dashboard.layouts.flash-message')
+        @include('flash::message')
 
         <div class="row">
-            <div class="col-12">
+            <div class="col-2"></div>
+            <div class="col-8">
                 <div class="card">
-                    <div class="card-body">
-                        <form action="{{route('notification.send')}}" method="post">
-                            @csrf
-                            <section class="container">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="card">
-                                            <div class="card-header">
-                                                <h4>{{ __('notification.sendNotification') }}</h4>
-                                            </div>
-                                            <div class="card-body">
-                                                <div class="mb-3 row">
-                                                    <label class="col-md-2 col-form-label">{{__('notification.type')}}</label>
-                                                    <div class="col-md-10">
-                                                        <select class="form-select" name="type" aria-label="Default select example">
-                                                            <option value="client">Client</option>
-                                                            <option value="car">Car</option>
-                                                            <option value="store">Store</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-            
-                                                <div class="mb-3 row">
-                                                    <label for="example-text-input" class="col-md-2 col-form-label">{{__('notification.title')}}</label>
-                                                    <div class="col-md-10">
-                                                        {!! Form::text('title',null,['class' => 'form-control', 'id' => 'title']) !!}
-                                                    </div>
-                                                </div>
-
-                                                <div class="mb-3 row">
-                                                    <label for="example-text-input" class="col-md-2 col-form-label">{{__('notification.body')}}</label>
-                                                    <div class="col-md-10">
-                                                        {!! Form::text('body',null,['class' => 'form-control', 'id' => 'body']) !!}
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="clearfix"></div>
-            
-                                            <div  style="margin: 0 0 30px 30px" class="form-group">
-                                                <button class="btn btn-primary submit-btn" >{{ __('notification.send') }}</button>
-                                            </div>
-                                        </div>
-                                    </div>
+                    <form action="{{route('notification.send')}}" method="post">
+                        @csrf
+                        <div class="card-header">
+                            <h4>{{ __('notification.sendNotification') }}</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="mb-3 row">
+                                <label class="col-md-3 col-form-label">{{__('notification.type')}}</label>
+                                <div class="col-md-8">
+                                    <select class="form-select" name="type" aria-label="Default select example">
+                                        <option value="client">{{ __('notification.client') }}</option>
+                                        <option value="car">{{ __('notification.captain') }}</option>
+                                        <option value="store">{{ __('notification.institution') }}</option>
+                                    </select>
                                 </div>
-                            </section>
-                        </form>
-                    </div>
+                            </div>
+
+                            <div class="mb-3 row">
+                                <label for="example-text-input" class="col-md-3 col-form-label">{{__('notification.title')}}</label>
+                                <div class="col-md-8">
+                                    {!! Form::text('title',null,['class' => 'form-control', 'id' => 'title']) !!}
+                                </div>
+                            </div>
+
+                            <div class="mb-3 row">
+                                <label for="example-text-input" class="col-md-3 col-form-label">{{__('notification.body')}}</label>
+                                <div class="col-md-8">
+                                    {!! Form::textarea('body',null,['class' => 'form-control', 'id' => 'body', 'rows'=> 3 ]) !!}
+                                </div>
+                            </div>
+
+                            <div class="mb-3 row">
+                                {!! Form::submit( __('notification.send'), ['class' => 'btn btn-primary submit-btn col-md-3'] ) !!}
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
             <!-- end col -->
@@ -89,28 +74,4 @@
         <!-- end row -->
 
     </div>
-@endsection
-@section('scripts')
-    <!-- Required datatable js -->
-    <script src="{{ asset('dashboard/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-
-    <!-- Buttons examples -->
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/jszip/jszip.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/pdfmake/build/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/pdfmake/build/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
-    <!-- Responsive examples -->
-    <script src="{{ asset('dashboard/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
-
-    <!-- Datatable init js -->
-    <script src="{{ asset('dashboard/js/pages/datatables.init.js') }}"></script>
-
-    {{-- <script src="{{ asset('dashboard/js/app.js') }}"></script> --}}
-
 @endsection
