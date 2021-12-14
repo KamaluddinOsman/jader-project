4b463b8acc435d70bf24a1c96996b0ef24e46259
Kamaluddin Osman <kamaluddin.osman@gmail.com>
2021-12-14 23:50:15 +0400

    Role to (test) stage

--- resources/views/dashboard/pages/role/index.blade.php
+++ resources/views/dashboard/pages/role/index.blade.php
@@ -6,13 +6,9 @@
     <!-- DataTables -->
     <link href="{{ asset('dashboard/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('dashboard/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
-    
+
     <!-- Sweet Alert-->
     <link href="{{ asset('dashboard/libs/sweetalert2/sweetalert.css') }}" rel="stylesheet" type="text/css" />
-
-    <!-- Responsive datatable examples -->
-    <link href="{{ asset('dashboard/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
-    {{-- <link rel="stylesheet" href="{{ asset('admin/plugins/sweetalert2/sweetalert.css') }}"> --}}
 @endsection
 @section('content')
     <div class="page-content">
@@ -34,11 +30,14 @@
         </div>
         <!-- end page title -->
 
+        @include('dashboard.layouts.flash-message')
+        @include('flash::message')
+        
         <div class="row">
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <a href="{{url('role/create')}}" class="btn btn-primary">
+                        <a href="{{url('role/create')}}" class="btn btn-primary" style="height: 40px">
                             <p>{{__('role.addRole')}}</p>
                         </a>
                     </div>
@@ -91,13 +90,6 @@
                                                 </form>
                                         </tr>
                                     @endforeach
-            
-                                @else
-            
-                                    <div class="alert alert-warning alert-block">
-                                        <strong>لا يوجد بيانات</strong>
-                                    </div>
-            
                                 @endif
                             </tbody>
 
@@ -117,78 +109,12 @@
     <script src="{{ asset('dashboard/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('dashboard/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
 
-    <!-- Buttons examples -->
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/jszip/jszip.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/pdfmake/build/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/pdfmake/build/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
-    <!-- Responsive examples -->
-    <script src="{{ asset('dashboard/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('dashboard/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
-
-    <!-- Datatable init js -->
-    {{-- <script src="{{ asset('dashboard/js/pages/datatables.init.js') }}"></script> --}}
-
     <!-- Sweet Alerts js -->
     <script src="{{ asset('dashboard/libs/sweetalert2/sweetalert.min.js') }}"></script>
 
     <!-- Sweet alert init js-->
     <script src="{{ asset('dashboard/js/pages/sweet-alerts.init.js') }}"></script>
 
-
-{{-- <script src="{{ asset('dashboard/js/custom.js') }}"></script> --}}
-
-    <script>
-
-        $(function () {
-            $("#datatable").DataTable({
-                "responsive": true, "lengthChange": false, "autoWidth": false,
-                "buttons": ["copy", "csv", "excel", "pdf", "print"]
-            }).buttons().container().appendTo('#datatable_wrapper .col-md-6:eq(0)');
-            $('#example2').DataTable({
-                "paging": true,
-                "lengthChange": false,
-                "searching": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "responsive": true,
-                "language": {
-                    "sProcessing": "جارٍ التحميل...",
-                    "sLengthMenu": "أظهر _MENU_ مدخلات",
-                    "sZeroRecords": "لم يعثر على أية سجلات",
-                    "sInfo": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
-                    "sInfoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
-                    "sInfoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
-                    "sInfoPostFix": "",
-                    "sSearch": "ابحث:",
-                    "sUrl": "",
-                    "oPaginate": {
-                        "sFirst": "الأول",
-                        "sPrevious": "السابق",
-                        "sNext": "التالي",
-                        "sLast": "الأخير"
-                    }
-                }
-            });
-        });
-
-        // Changing Category Status
-        $('.activeCheck').change(function () {
-            var url = this.getAttribute('data-url');
-            var token = this.getAttribute('data-token');
-            $.ajax({
-                type: 'get',
-                data: {_token: token},
-                url: url,
-            });
-            location.href = "/client";
-        });
-
-    </script>
-
+    <!-- Datatable init js -->
+    <script src="{{ asset('dashboard/js/pages/datatables.init.js') }}"></script>
 @endsection
